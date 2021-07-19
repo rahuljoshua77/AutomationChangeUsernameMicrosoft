42ff1823f6521765e3dc587bd46e7591b952fa5a
Rahul Joshua Damanik <73378179+rahuljoshua77@users.noreply.github.com>
2021-07-19 11:54:40 +0700

    Update microsoft_change_user.py

--- microsoft_change_user.py
+++ microsoft_change_user.py
@@ -35,15 +35,15 @@ def get_random_string(length):
 def manage_account():
     # try:
     
-    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="viewport"]/div[5]/div/div/div/div[2]/div/div/div/div/div/div[1]/div/div/div[2]/div[1]/div'))).click()
+    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, "(//button[@data-automation-id='MoreActionsQuick'])[1]"))).click()
     print(f"[*] [ {email} ] Success Login")
     print(f"[*] [ {email} ] Trying to Change Username")
     sleep(1)
-    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div/div/div/div/div[3]/div/div[3]/div/div[2]/div/div/div/div[1]/div/div[1]/div/div[3]/div/div/button'))).click()
+    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Manage username and email')]"))).click()
 
     sleep(1)
-    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[6]/div/div/div/div[2]/div[3]/div/div[3]/div[2]/div[2]/div[2]/div/button'))).click()
-    username = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[6]/div/div/div/div[2]/div[3]/div/div[3]/div[2]/div[2]/div/div/div[1]/div/div/input')))
+    wait(browser,30).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-automation-id="UserListV2,ManageUserEmailAliasesRegion,showPrimaryUPNEditStateIconButtonBtn"]')).click()
+    username = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, '//input[@aria-label="Primary UPN"]')))
     try:
         username.send_keys(Keys.CONTROL + "a")
         username.send_keys(Keys.DELETE)
@@ -51,12 +51,12 @@ def manage_account():
         pass
 
     username.send_keys(f"{get_random_string(5)}{random.randint(10,99)}{get_random_string(35)}")
-    wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[6]/div/div/div/div[2]/div[3]/div/div[3]/div[2]/div[2]/div/div/button"))).click()
+    wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Done')]"))).click()
     try:
-        username_new = wait(browser,25).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div/div/div[2]/div[3]/div/div[3]/div[2]/div[2]/div[1]"))).text
+        username_new = wait(browser,25).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class,'primaryUPNReadOnlyText')]"))).text
         print(f"[*] [ {email} ] Username Change to [ {username_new} ]")
-        wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[6]/div/div/div/div[2]/div[3]/div[3]/div/div/button"))).click()
-        get_notif = wait(browser,45).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div/div/div[2]/div[3]/div[2]/div[2]/div[1]/div/div/div[2]/span/span"))).text
+        wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Save changes')]"))).click()
+        get_notif = wait(browser,45).until(EC.presence_of_element_located((By.XPATH, "//span[contains(@class,'ms-MessageBar-innerText innerText-')]"))).text
         print(f"[*] [ {email} ] Status: {get_notif}")
         
         with open('success.txt','a') as f:
@@ -88,18 +88,18 @@ def open_browser(k):
     browser.get('https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users')
 
     print(f"[*] [ {email} ] Trying to Login")
-    element = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div[1]/div[3]/div/div/div/div[2]/div[2]/div/input[1]")))
+    element = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, "//input[@type='email']")))
     element.send_keys(email)
         
     sleep(0.5)
     element.send_keys(Keys.ENTER) 
-    element = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[2]/div/div[2]/input")))
+    element = wait(browser,15).until(EC.presence_of_element_located((By.XPATH, "//input[@type='password']")))
     element.send_keys(password)
         
     sleep(0.5)
     element.send_keys(Keys.ENTER) 
     try:
-        wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/form/div/div/div[1]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div[1]/input"))).click()
+        wait(browser,10).until(EC.element_to_be_clickable((By.XPATH, "//input[contains(@value,'Yes')]"))).click()
 
     except:
         pass
